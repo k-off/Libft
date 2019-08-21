e23bd63e2dd284f594211ce5dc1cb27ee20dc169
Pavel Covaliov <pacovali@f1r3s19.codam.nl>
2019-08-21 09:38:10 +0200

    simple gnl with multiple fd-s

--- inpout/get_next_line.c
+++ inpout/get_next_line.c
@@ -14,8 +14,8 @@
 
 int			ft_get_line(char **line, char **lines, int i)
 {
-	int		j;
-
+	int				j;
+	
 	j = i;
 	if (i == -1)
 		return (-1);
@@ -24,9 +24,10 @@ int			ft_get_line(char **line, char **lines, int i)
 	while (line[0][i] != '\n' && line[0][i] != '\0')
 		i++;
 	if (line[0][i] != '\n' && j != -2)
+	{
 		free(line[0]);
-	if (line[0][i] != '\n' && j != -2)
 		return (0);
+	}
 	free(lines[0]);
 	if (line[0][i] == '\n')
 		lines[0] = ft_strdup(&(line[0][i + 1]));
@@ -42,9 +43,9 @@ int			ft_set_line(int fd, char **lines)
 {
 	char			buf[BUFF_SIZE + 1];
 	char			*tmp;
-	long int		r;
+	long			r;
 	int				i;
-
+	
 	r = 1;
 	while (r != 0)
 	{
@@ -66,16 +67,18 @@ int			ft_set_line(int fd, char **lines)
 
 int			get_next_line(const int fd, char **line)
 {
-	static	char	*lines;
+	static	char	**lines;
 	int				gl_ret;
-
+	
 	if (fd < 0 || !line || BUFF_SIZE < 1)
 		return (-1);
 	if (!lines)
-		lines = ft_strdup("\0");
-	gl_ret = ft_get_line(line, &lines, 0);
+		lines = (char**)ft_memalloc(sizeof(char*) * 1001);
+	if (lines[fd] == 0)
+		lines[fd] = ft_strdup("\0");
+	gl_ret = ft_get_line(line, &lines[fd], 0);
 	if (gl_ret == 0)
-		gl_ret = ft_get_line(line, &lines, ft_set_line(fd, &lines));
+		gl_ret = ft_get_line(line, &(lines[fd]), ft_set_line(fd, &(lines[fd])));
 	if (gl_ret > 0)
 		return (1);
 	else if (gl_ret < 0)
